6e82846b5991209903971ca6d1513da1cb933bd6
coderx <36203292+blcx@users.noreply.github.com>
2020-11-11 21:23:34 +0530

    telegram update

--- main.py
+++ main.py
@@ -6,14 +6,24 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 import time
 import json
-import schedule
 import undetected_chromedriver as uc
+import telegram
+
+
+with open('TelegramBot.json', 'r') as file:
+    botinfo = json.load(file)
+    file.close()
+
+token = botinfo['TOKEN']
+id = botinfo['CHATID']
+bot = telegram.Bot(token=token)
 
 counter = 0
 global claim
 
 def visit_site_xrp():
     print("Visiting site xrp")
+    bot.send_message(chat_id=id, text="Visiting XRP site")
     with open('Login.json', 'r') as file:
         login = json.load(file)
         file.close()
@@ -24,6 +34,7 @@ def visit_site_xrp():
     #driver = webdriver.Chrome(PATH)
     driver = uc.Chrome()
     driver.set_page_load_timeout(120)
+
     try:
         driver.get("https://coinfaucet.io/free")
     except TimeoutException as e:
@@ -36,6 +47,12 @@ def visit_site_xrp():
         # click roll button if it is present
         driver.find_element_by_xpath("/html/body/main/div/div/div/div/div/div[5]/button").click()
         print("roll was successful on page load")
+        time.sleep(5)
+
+        #send msg to telegram bot
+        element = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.CLASS_NAME, "result")))
+        bot.send_message(chat_id=id, text=element.text)
+
         time.sleep(2)
         driver.quit()
 
@@ -52,12 +69,18 @@ def visit_site_xrp():
             time.sleep(5)
             driver.find_element_by_xpath("/html/body/main/div/div/div/div/div/div[5]/button").click()
             print("roll success clicked after login")
+            time.sleep(5)
+            # send msg to telegram bot
+            element = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.CLASS_NAME, "result")))
+            bot.send_message(chat_id=id, text=element.text)
+
             claim += 1
             time.sleep(2)
             driver.quit()
 
         except:
             print("already clicked wait more minutes")
+            bot.send_message(chat_id=id, text="Already Claimed")
             time.sleep(2)
             driver.quit()
     print("Visited site XRP successfully")
@@ -65,6 +88,7 @@ def visit_site_xrp():
 
 def visit_site_cardano():
     print("Visiting site cardano")
+    bot.send_message(chat_id=id, text="Visiting site Cardano")
     with open('Login.json', 'r') as file:
         login = json.load(file)
         file.close()
@@ -86,6 +110,7 @@ def visit_site_cardano():
         # click roll button if it is present
         driver.find_element_by_xpath("/html/body/main/div/div/div/div/div/div[5]/button").click()
         print("roll was successful on page load")
+
         time.sleep(2)
         driver.quit()
 
@@ -102,18 +127,23 @@ def visit_site_cardano():
             time.sleep(5)
             driver.find_element_by_xpath("/html/body/main/div/div/div/div/div/div[5]/button").click()
             print("roll success clicked after login")
+
             claim += 1
             time.sleep(2)
             driver.quit()
 
         except:
             print("already clicked wait more minutes")
+            bot.send_message(chat_id=id, text="Already Claimed")
             time.sleep(2)
             driver.quit()
     print("Visited site Cardano successfully")
 
+
 def visit_site_tron():
     print("Visiting site Tron")
+    bot.send_message(chat_id=id, text="Visiting site Tron")
+
     with open('Login.json', 'r') as file:
         login = json.load(file)
         file.close()
@@ -136,6 +166,12 @@ def visit_site_tron():
         driver.find_element_by_xpath("/html/body/main/div/div/div/div/div/div[5]/button").click()
         print("roll was successful on page load")
         time.sleep(5)
+
+        # send msg to telegram bot
+        element = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.CLASS_NAME, "result")))
+        bot.send_message(chat_id=id, text=element.text)
+
+        time.sleep(5)
         driver.quit()
 
     except:
@@ -151,12 +187,20 @@ def visit_site_tron():
             time.sleep(5)
             driver.find_element_by_xpath("/html/body/main/div/div/div/div/div/div[5]/button").click()
             print("roll success clicked after login")
+
+            time.sleep(5)
+
+            # send msg to telegram bot
+            element = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.CLASS_NAME, "result")))
+            bot.send_message(chat_id=id, text=element.text)
+
             claim += 1
             time.sleep(5)
             driver.quit()
 
         except:
             print("already clicked wait more minutes")
+            bot.send_message(chat_id=id, text="Already Claimed")
             time.sleep(5)
             driver.quit()
     print("Visited site Tron successfully")
@@ -164,6 +208,8 @@ def visit_site_tron():
 
 def visit_site_dash():
     print("Visiting site Dash")
+    bot.send_message(chat_id=id, text="Visiting site Dash")
+
     with open('Login.json', 'r') as file:
         login = json.load(file)
         file.close()
@@ -185,6 +231,13 @@ def visit_site_dash():
         # click roll button if it is present
         driver.find_element_by_xpath("/html/body/main/div/div/div/div/div/div[5]/button").click()
         print("roll was successful on page load")
+
+        time.sleep(5)
+
+        # send msg to telegram bot
+        element = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.CLASS_NAME, "result")))
+        bot.send_message(chat_id=id, text=element.text)
+
         time.sleep(5)
         driver.quit()
 
@@ -201,18 +254,30 @@ def visit_site_dash():
             time.sleep(5)
             driver.find_element_by_xpath("/html/body/main/div/div/div/div/div/div[5]/button").click()
             print("roll success clicked after login")
+
+            time.sleep(5)
+
+            # send msg to telegram bot
+            element = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.CLASS_NAME, "result")))
+            bot.send_message(chat_id=id, text=element.text)
+
             claim += 1
             time.sleep(5)
             driver.quit()
 
         except:
             print("already clicked wait more minutes")
+            bot.send_message(chat_id=id, text="Already Claimed")
             time.sleep(5)
             driver.quit()
     print("Visited site Dash successfully")
 
+
+
 def visit_site_eth():
     print("Visiting site ETH")
+    bot.send_message(chat_id=id, text="Visiting site ETH")
+
     with open('Login.json', 'r') as file:
         login = json.load(file)
         file.close()
@@ -234,6 +299,13 @@ def visit_site_eth():
         # click roll button if it is present
         driver.find_element_by_xpath("/html/body/main/div/div/div/div/div/div[5]/button").click()
         print("roll was successful on page load")
+
+        time.sleep(5)
+
+        # send msg to telegram bot
+        element = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.CLASS_NAME, "result")))
+        bot.send_message(chat_id=id, text=element.text)
+
         time.sleep(5)
         driver.quit()
 
@@ -250,19 +322,30 @@ def visit_site_eth():
             time.sleep(5)
             driver.find_element_by_xpath("/html/body/main/div/div/div/div/div/div[5]/button").click()
             print("roll success clicked after login")
+
+            time.sleep(5)
+
+            # send msg to telegram bot
+            element = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.CLASS_NAME, "result")))
+            bot.send_message(chat_id=id, text=element.text)
+
             claim += 1
             time.sleep(5)
             driver.quit()
 
         except:
             print("already clicked wait more minutes")
+            bot.send_message(chat_id=id, text="Already Claimed")
             time.sleep(5)
             driver.quit()
     print("Visited site ETH successfully")
 
 
+
 def visit_site_nem():
     print("Visiting site nem")
+    bot.send_message(chat_id=id, text="Visiting site NEM")
+
     with open('Login.json', 'r') as file:
         login = json.load(file)
         file.close()
@@ -284,6 +367,13 @@ def visit_site_nem():
         # click roll button if it is present
         driver.find_element_by_xpath("/html/body/main/div/div/div/div/div/div[5]/button").click()
         print("roll was successful on page load")
+
+        time.sleep(5)
+
+        # send msg to telegram bot
+        element = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.CLASS_NAME, "result")))
+        bot.send_message(chat_id=id, text=element.text)
+
         time.sleep(5)
         driver.quit()
 
@@ -300,19 +390,29 @@ def visit_site_nem():
             time.sleep(5)
             driver.find_element_by_xpath("/html/body/main/div/div/div/div/div/div[5]/button").click()
             print("roll success clicked after login")
+
+            time.sleep(5)
+
+            # send msg to telegram bot
+            element = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.CLASS_NAME, "result")))
+            bot.send_message(chat_id=id, text=element.text)
+
             claim += 1
             time.sleep(5)
             driver.quit()
 
         except:
             print("already clicked wait more minutes")
+            bot.send_message(chat_id=id, text="Already Claimed")
             time.sleep(5)
             driver.quit()
     print("Visited site NEM successfully")
 
 
+
 def visit_site_neo():
     print("Visiting site neo")
+    bot.send_message(chat_id=id, text="Visiting site NEO")
     with open('Login.json', 'r') as file:
         login = json.load(file)
         file.close()
@@ -334,6 +434,13 @@ def visit_site_neo():
         # click roll button if it is present
         driver.find_element_by_xpath("/html/body/main/div/div/div/div/div/div[5]/button").click()
         print("roll was successful on page load")
+
+        time.sleep(5)
+
+        # send msg to telegram bot
+        element = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.CLASS_NAME, "result")))
+        bot.send_message(chat_id=id, text=element.text)
+
         time.sleep(5)
         driver.quit()
 
@@ -350,25 +457,37 @@ def visit_site_neo():
             time.sleep(5)
             driver.find_element_by_xpath("/html/body/main/div/div/div/div/div/div[5]/button").click()
             print("roll success clicked after login")
+
+            time.sleep(5)
+
+            # send msg to telegram bot
+            element = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.CLASS_NAME, "result")))
+            bot.send_message(chat_id=id, text=element.text)
+
             claim += 1
             time.sleep(5)
             driver.quit()
 
         except:
             print("already clicked wait more minutes")
+            bot.send_message(chat_id=id, text="Already Claimed")
             time.sleep(5)
             driver.quit()
     print("Visited site NEO successfully")
 
 
+
 def visit_site_link():
     print("Visiting site link")
+    bot.send_message(chat_id=id, text="Visiting site LINK")
+
     with open('Login.json', 'r') as file:
         login = json.load(file)
         file.close()
 
     username = login['userLINK']
     password = login['passLINK']
+
     #driver = webdriver.Chrome(PATH)
     driver = uc.Chrome()
     driver.set_page_load_timeout(120)
@@ -384,6 +503,13 @@ def visit_site_link():
         # click roll button if it is present
         driver.find_element_by_xpath("/html/body/main/div/div/div/div/div/div[5]/button").click()
         print("roll was successful on page load")
+
+        time.sleep(5)
+
+        # send msg to telegram bot
+        element = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.CLASS_NAME, "result")))
+        bot.send_message(chat_id=id, text=element.text)
+
         time.sleep(5)
         driver.quit()
 
@@ -400,15 +526,24 @@ def visit_site_link():
             time.sleep(5)
             driver.find_element_by_xpath("/html/body/main/div/div/div/div/div/div[5]/button").click()
             print("roll success clicked after login")
+
+            time.sleep(5)
+
+            # send msg to telegram bot
+            element = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.CLASS_NAME, "result")))
+            bot.send_message(chat_id=id, text=element.text)
+
             claim += 1
             time.sleep(5)
             driver.quit()
 
         except:
             print("already clicked wait more minutes")
+            bot.send_message(chat_id=id, text="Already Claimed")
             time.sleep(5)
             driver.quit()
     print("Visited site LINK successfully")
+
 
 
 while True:
@@ -429,11 +564,12 @@ while True:
    visit_site_link()
    time.sleep(5)
 
-
+   bot.send_message(chat_id=id, text="Waiting About an Hour...")
    print("waiting about an hour")
    time.sleep(3600) #seconds to hour
    counter += 1
-   print("wait finished " + "Run Times: " + str(counter))
+   bot.send_message(chat_id=id, text="wait finished " + "Run Times: " + str(counter))
+   print("Wait Finished " + "Run Times: " + str(counter))
 
 
 

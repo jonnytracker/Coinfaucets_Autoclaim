647c200aca8ff9706fc668ba29818fe8a77df861
coderx <36203292+blcx@users.noreply.github.com>
2020-11-15 15:18:36 +0530

    added BNB

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
-import time
+import time, random
 import json
 import undetected_chromedriver as uc
 import telegram
@@ -758,31 +758,161 @@ def visit_site_link():
     driver.quit()
 
 
+def visit_site_binanceCoin():
+    print("Visiting site BinanceCoin")
+    bot.send_message(chat_id=id, text="Visiting site BinanceCoin")
+
+    with open('Login.json', 'r') as file:
+        login = json.load(file)
+        file.close()
+
+    username = login['userBNB']
+    password = login['passBNB']
+
+    opts = uc.ChromeOptions()
+    opts.add_argument('--disable-notifications')
+    # opts.add_extension("extension_3_9_5_0.crx")
+    driver = uc.Chrome(options=opts)
+    driver.set_page_load_timeout(120)
+
+    try:
+        driver.get("https://freebinancecoin.com/")
+    except TimeoutException as e:
+        print(e.msg)
+        print("Page load Timeout Occured. Quiting !!!")
+        driver.quit()
+
+    try:
+        time.sleep(10)
+        # click roll button if it is present
+        driver.find_element_by_xpath("/html/body/main/div/div/div/div/div/div[5]/button").click()
+        print("roll was successful on page load")
+
+        time.sleep(5)
+
+        # send msg to telegram bot
+        element = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.CLASS_NAME, "result")))
+        bot.send_message(chat_id=id, text=element.text)
+
+        time.sleep(5)
+        driver.quit()
+
+    except:
+        # if roll button not present try to login
+        element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "email")))
+        element.send_keys(username)
+        element = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.NAME, "password")))
+        element.send_keys(password)
+
+        # try login click if fail close the ads iframe
+        try:
+            # try login click
+            driver.find_element_by_xpath("//button[contains(text(),'LOGIN!')]").click()
+        except:
+            print("login button click fail")
+            # close ads since login fail
+            driver.find_element_by_xpath("/html/body/div[1]/div[1]").click()
+
+            try:
+                # try login after ads close
+                print("trying login again")
+                driver.find_element_by_xpath("//button[contains(text(),'LOGIN!')]").click()
+                print("login successful")
+            except:
+                # if fail again close
+                print("login fail quitting ")
+                time.sleep(5)
+                driver.quit()
+
+    try:
+        time.sleep(5)
+        driver.find_element_by_xpath("/html/body/main/div/div/div/div/div/div[5]/button").click()
+        print("roll success clicked after login")
+
+        time.sleep(5)
+
+        # send msg to telegram bot
+        element = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.CLASS_NAME, "result")))
+        bot.send_message(chat_id=id, text=element.text)
+
+        claim += 1
+        time.sleep(5)
+        driver.quit()
+
+    except:
+        print("already clicked wait more minutes")
+        bot.send_message(chat_id=id, text="Already Claimed")
+        time.sleep(5)
+        driver.quit()
+
+    print("Visited site BinanceCoin successfully")
+    driver.quit()
+
+
+
 
 while True:
-   visit_site_xrp()
-   time.sleep(5)
-   visit_site_cardano()
-   time.sleep(5)
-   visit_site_tron()
-   time.sleep(5)
-   visit_site_dash()
-   time.sleep(5)
-   visit_site_eth()
-   time.sleep(5)
-   visit_site_nem()
-   time.sleep(5)
-   visit_site_neo()
-   time.sleep(5)
-   visit_site_link()
-   time.sleep(5)
+    rand = random.randrange(1,10)   
+    time.sleep(rand)
 
-   bot.send_message(chat_id=id, text="Waiting About an Hour...")
-   print("waiting about an hour")
-   time.sleep(3600) #seconds to hour
-   counter += 1
-   bot.send_message(chat_id=id, text="wait finished " + "Run Times: " + str(counter))
-   print("Wait Finished " + "Run Times: " + str(counter))
+    visit_site_xrp()
+
+    rand = random.randrange(1,10)   
+    time.sleep(rand)
+
+    visit_site_cardano()
+
+    rand = random.randrange(1,10)   
+    time.sleep(rand)
+
+    visit_site_tron()
+
+    rand = random.randrange(1,10)   
+    time.sleep(rand)
+
+    rand = random.randrange(1,10)   
+    time.sleep(rand)
+
+    visit_site_dash()
+
+    rand = random.randrange(1,10)   
+    time.sleep(rand)
+
+    visit_site_eth()
+
+    rand = random.randrange(1,10)   
+    time.sleep(rand)
+
+    visit_site_nem()
+
+    rand = random.randrange(1,10)   
+    time.sleep(rand)
+
+    visit_site_neo()
+
+    rand = random.randrange(1,10)   
+    time.sleep(rand)
+
+    visit_site_link()
+
+    rand = random.randrange(1,10)   
+    time.sleep(rand)
+
+    visit_site_binanceCoin()
+
+    rand = random.randrange(1,10)   
+    time.sleep(rand)
+
+    bot.send_message(chat_id=id, text="Waiting About an Hour...")
+    print("waiting about an hour")
+
+    rand = random.randrange(3600,9000)
+    
+    time.sleep(rand) #seconds to hour
+
+    counter += 1
+    bot.send_message(chat_id=id, text="wait finished " + "Run Times: " + str(counter))
+    print("Wait Finished " + "Run Times: " + str(counter))
 
 
 
